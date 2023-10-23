bed7ae746145e1b25c8c0bbcce6d49a49bced30f
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2023-10-23 18:31:28 +0500

    Add nav styles

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ onMounted(() => {
   <!-- Heading -->
   <header>
     <img src="./assets/pinia-logo.svg" alt="Pinia logo">
-    <h1>Pinia Tasks</h1>
+    <h1>Pinia TodoList</h1>
   </header>
 
   <!-- New Task Form -->
@@ -28,8 +28,8 @@ onMounted(() => {
 
   <!-- Filter Nav -->
   <nav class="filter">
-    <button @click="filter = 'all'">All Tasks</button>
-    <button @click="filter = 'favs'">Favorite Tasks</button>
+    <button :class="{ active: filter == 'all' }" @click=" filter = 'all'">All Tasks</button>
+    <button :class="{ active: filter == 'favs' }" @click=" filter = 'favs'">Favorite Tasks</button>
   </nav>
 
   <div v-if="Loading" class="loading">
@@ -39,14 +39,14 @@ onMounted(() => {
   <!-- Task List -->
   <div class="task-list" v-if="filter == 'all'">
     <p>You have {{ totalCount }} tasks left to do</p>
-    <div v-for="task in tasks" :key="task.id">
+    <div v-for=" task  in  tasks " :key="task.id">
       <TaskDetails :task="task" />
     </div>
   </div>
 
   <div class="task-list" v-if="filter == 'favs'">
     <p>You have {{ favCount }} fav left to do</p>
-    <div v-for="task in favs" :key="task.id">
+    <div v-for=" task  in  favs " :key="task.id">
       <TaskDetails :task="task" />
     </div>
   </div>
